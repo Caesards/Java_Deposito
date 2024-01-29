ad07307ad734ad8d9939c1e4fb5ced4202f9a134
cesar <cesardsalas@gmail.com>
2024-01-29 20:49:08 +0100

    Ejercicio Acabado

--- src/Cuentas/Main.java
+++ src/Cuentas/Main.java
@@ -1,28 +1,52 @@
 package Cuentas;
 
+/**
+ * Este es el constructor principal para que se inice la aplicacion.
+ */
 public class Main {
 
-    public static void main(String[] args) {
-        operativa_cuenta(100);
+    /**
+     * Es el constructor por defecto de la clase main.*/
 
+    public Main() {
+        // aqui podemos no implementar nada si es necesario
     }
 
-    public static void operativa_cuenta(float cantidad){
-        CCuenta cuenta1;
+    /**
+     * Método principal de la aplicación.
+     * Este método se ejecuta al iniciar la aplicación.
+     *
+     * @param args Los argumentos de línea de comandos pasados a la aplicación.
+     */
+
+
+    public static void main(String[] args) {
+        // Aquí va la lógica principal de tu aplicación
+        operativa_cuenta();
+    }
+
+    /**
+     * Aqui se simula las operaciones bancarias:
+     * primero se crea una cuenta
+     * Segundo se retira dinero
+     * tercero se ingresa dinero
+     */
+    public static void operativa_cuenta(){
+        CCuenta Cuenta1;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
+        Cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = Cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
         try {
-            cuenta1.retirar(2300);
+            Cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
+            Cuenta1.ingresar(695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
